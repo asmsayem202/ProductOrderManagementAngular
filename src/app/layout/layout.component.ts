78e67ed77f06cd74e5fca33b753f9c1b83fa096d
Asm Sayem <Asm Sayem@DESKTOP-5C486UG>
2025-07-20 16:24:47 +0600

    feat: add layout design

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterModule],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css'],
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  toggleSidebar = true;
+  dropdownOpen = false;
+
+  @ViewChild('dropdownRef') dropdownRef!: ElementRef;
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    if (
+      this.dropdownOpen &&
+      this.dropdownRef &&
+      !this.dropdownRef.nativeElement.contains(event.target)
+    ) {
+      this.dropdownOpen = false;
+    }
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    window.location.href = '/login'; // or use router.navigate(['/login']);
+  }
+}
