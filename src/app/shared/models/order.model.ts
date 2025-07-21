5ae819629fac9da367189ba32f4265aa6d723596
Asm Sayem <Asm Sayem@DESKTOP-5C486UG>
2025-07-21 12:12:48 +0600

    feat: order model and services

---
+++ src/app/shared/models/order.model.ts
@@ -0,0 +1,30 @@
+export interface CreateOrderItem {
+  productId: number;
+  variantId: number;
+  quantity: number;
+}
+
+export interface CreateOrder {
+  customerName: string;
+  customerEmail: string;
+  customerAddress: string;
+  items: CreateOrderItem[];
+}
+
+export interface OrderItem {
+  id: number;
+  productName: string;
+  variantColor: string;
+  variantSpecification: string;
+  variantSize: 'Small' | 'Medium' | 'Large';
+  quantity: number;
+}
+
+export interface Order {
+  id: number;
+  customerName: string;
+  customerEmail: string;
+  customerAddress: string;
+  orderDate?: string;
+  items: OrderItem[];
+}
--- src/app/shared/services/order.service.ts
+++ src/app/shared/services/order.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CreateOrder, Order } from '../models/order.model';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService {
+  private baseUrl = `${environment.apiUrl}/Orders`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getOrders(): Observable<Order[]> {
+    return this.http.get<Order[]>(this.baseUrl);
+  }
+
+  getOrder(id: number): Observable<Order> {
+    return this.http.get<Order>(`${this.baseUrl}/${id}`);
+  }
+
+  createOrder(order: CreateOrder): Observable<Order> {
+    return this.http.post<Order>(this.baseUrl, order);
+  }
+
+  updateOrder(id: number, order: CreateOrder): Observable<void> {
+    return this.http.put<void>(`${this.baseUrl}/${id}`, order);
+  }
+
+  deleteOrder(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
 }
